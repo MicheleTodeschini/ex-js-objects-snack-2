b8d3cc67f8e4776c49194c2a6e073a374af35530
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-03-10 15:23:17 +0100

    solved es 2 and explained

--- es 2/main.js
+++ es 2/main.js
@@ -1,1 +1,19 @@
 console.log('funzia');
+/* 
+P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
+Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? */
+
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = { ...hamburger };
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // salad
+console.log(secondBurger.ingredients[0]); // Salad
+
+//Con la shallow copy (spread operator) il contenuto cambia perché le proprietà annidate vengono copiate come irferimetno, ma solo delle proprietà annidate!
