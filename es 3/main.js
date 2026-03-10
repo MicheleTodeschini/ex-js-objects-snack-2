04e8d45ca77d7721264a2b155cda3dc5ba002e9d
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-03-10 15:26:02 +0100

    solved es 3 and explained

--- es 3/main.js
+++ es 3/main.js
@@ -1,1 +1,22 @@
 console.log('funzia');
+
+/* Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? */
+
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29
+    }
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger);
+
+// viene creata una copia con anche gli oggetti complessi con secondBurger e una copia con oggetti complessi con ThirdBurger
